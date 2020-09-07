4cc6cd5935b3532f49dfa089fa8125495e0e0bd7
stalker <stalkersmail@rambler.ru>
2020-09-08 00:40:55 +0400

    Enabled security and added login page

---
+++ src/main/java/com/xtech/taskwatch/config/ViewResolverConfig.java
@@ -0,0 +1,17 @@
+package com.xtech.taskwatch.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ViewResolverConfig {
+
+    /*@Bean
+    public ViewResolver getResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/jsp/", ".jsp");
+        //resolver.setPrefix("/WEB-INF/view/");
+
+        
+
+        return resolver;
+    }*/
+}
